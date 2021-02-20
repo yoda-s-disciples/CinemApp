43d21d32e6c920df664864b5c6920c9e0d1b0749
camilosaenz <rubiansaenz1997@gmail.com>
2021-02-20 15:35:48 -0500

    Update

--- src/main/java/edu/eci/arsw/cinemApp/CinemAppApplication.java
+++ src/main/java/edu/eci/arsw/cinemApp/CinemAppApplication.java
@@ -8,6 +8,7 @@ public class CinemAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemAppApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
