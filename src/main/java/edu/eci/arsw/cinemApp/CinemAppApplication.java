b5f3c2cfd35a088364072f20b2ae15438579674b
camilosaenz <rubiansaenz1997@gmail.com>
2021-02-20 13:16:13 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/cinemApp/CinemAppApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.cinemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemAppApplication.class, args);
+	}
+
+}
